e97fd5517d8a3b75e2557c3dc554ae79e207e3c1
Harsh Aggarwal <harshagg21521@gmail.com>
2026-09-22 23:14:15 +0530

    added theme

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,6 +2,7 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
 
 import { routes } from './app.routes';
 
@@ -11,6 +12,9 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideAnimationsAsync(),
     providePrimeNG({ 
+        theme: {
+            preset: Aura
+        }
     })
   ],
 };
